c21981070c7a964618adc585601626f71c10ae96
Callum Bonnyman <callum@wrux.com>
2022-11-24 01:16:22 +0900

    Fixed compression removes spaces around inline text elements.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,21 @@ import image from '@astrojs/image';
 
 export default defineConfig({
   integrations: [
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    tailwind(),
     compress({
       path: ['./build', './dist'],
       logger: 0,
       html: {
         collapseInlineTagWhitespace: true,
         collapseWhitespace: true,
+        conservativeCollapse: true,
         decodeEntities: true,
         html5: true,
         removeComments: true,
       },
     }),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    tailwind(),
   ],
 });
