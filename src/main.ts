7625f7a97278ee0e61b239c95d32830f5e45c5eb
Callum Bonnyman <callum@wrux.com>
2022-11-23 16:18:52 +0900

    Added timeline.

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,43 @@ import './main.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
-document.querySelectorAll('[data-work]')?.forEach((item) => {
-  const jobTimeline = gsap.timeline({
+const gsapMedia = gsap.matchMedia();
+
+gsapMedia.add('(min-width: 768px)', () => {
+  const jobStackTimeline = gsap.timeline({
     scrollTrigger: {
-      trigger: item,
-      start: 'top 80%',
-      end: 'bottom 65%',
+      trigger: document.querySelector('.jobs'),
+      start: 'top 50%',
+      end: 'bottom 30%',
       scrub: 1,
     },
   });
+  jobStackTimeline
+    .addLabel('start')
+    .fromTo(
+      document.querySelector('.jobs .job-stack__line'),
+      { height: '0%' },
+      { height: '100%' }
+    );
 
-  jobTimeline
-    .addLabel('showAll')
-    .from(item.querySelectorAll('[data-work-item]'), {
-      y: '50%',
-      opacity: 0,
-      scale: 0.9,
-      // skew: skew(0deg, 3deg)
-      skewY: '5deg',
-      stagger: 0.25,
+  document.querySelectorAll('[data-work]')?.forEach((item) => {
+    const jobTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: item,
+        start: 'top 80%',
+        end: 'bottom 65%',
+        scrub: 1,
+      },
     });
+
+    jobTimeline
+      .addLabel('showAll')
+      .from(item.querySelectorAll('[data-work-item]'), {
+        y: '50%',
+        opacity: 0,
+        scale: 0.9,
+        skewY: '5deg',
+        stagger: 0.25,
+      });
+  });
 });
