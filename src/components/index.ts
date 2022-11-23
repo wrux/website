05a72a065b4c2cfa0ff291ce7df20d20317b4388
Callum <callumbonnyman@me.com>
2022-11-24 03:09:33 +0900

    Added tracking proxy service. (#12)

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as ExternalLink } from './ExternalLink.astro';
+export { default as Icon } from './Icon.astro';
+export { default as Jobs } from './Jobs.astro';
+export { default as Links } from './Links.astro';
+export { default as Logo } from './Logo.astro';
+export { default as Panel } from './Panel.astro';
+export { default as Paragraph } from './Paragraph.astro';
