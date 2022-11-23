d561ddef67afa118f4aeebcbdbb9e7a94328cc48
Callum Bonnyman <callum@wrux.com>
2022-11-24 00:45:10 +0900

    Added basic post-build compression.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
+    compress({
+      path: ['./build', './dist'],
+      logger: 0,
+      html: {
+        collapseInlineTagWhitespace: true,
+        collapseWhitespace: true,
+        decodeEntities: true,
+        html5: true,
+        removeComments: true,
+      },
+    }),
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
     }),
