cbd70d721fc0bd05c8941641e549003fab65d3d0
Callum Bonnyman <callum@wrux.com>
2023-06-09 14:53:06 +0545

    Cleaned up codebase.

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as ExternalLink } from './ExternalLink.astro';
-export { default as Icon } from './Icon.astro';
-export { default as Jobs } from './Jobs.astro';
-export { default as Links } from './Links.astro';
-export { default as Logo } from './Logo.astro';
-export { default as Panel } from './Panel.astro';
-export { default as Paragraph } from './Paragraph.astro';
