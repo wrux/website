368e282b962234511c01db4b1ef8e20744fe94b3
Callum Bonnyman <callum@wrux.com>
2024-11-04 12:15:39 +0700

    Added Vercel analytics.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/static';
+
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
