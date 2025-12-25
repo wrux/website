3ac1df304896e67afc0beb745ef78e0633d7b6cd
Callum Bonnyman <callum@wrux.com>
2025-12-25 22:34:34 +0300

    Updated deps and remove dead code.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'static',
