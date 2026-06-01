753cf33dd27e87f232db47d0bc1a79c56580bc29
Callum Bonnyman <callum@wrux.com>
2026-06-01 23:04:33 +0100

    Optimize CSS build and print styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 import vercel from '@astrojs/vercel';
@@ -12,10 +11,7 @@ export default defineConfig({
     },
   }),
   build: {
-    inlineStylesheets: 'always',
-  },
-  vite: {
-    plugins: [tailwindcss()],
+    inlineStylesheets: 'never',
   },
   integrations: [
     icon({
---
+++ postcss.config.cjs
@@ -0,0 +1,26 @@
+const production = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  plugins: [
+    require('postcss-preset-env')({
+      stage: 3,
+      preserve: false,
+      autoprefixer: false,
+      features: {
+        'custom-media-queries': true,
+        'gap-properties': false,
+        'nesting-rules': true,
+      },
+    }),
+    production &&
+      require('cssnano')({
+        preset: [
+          'default',
+          {
+            discardComments: { removeAll: true },
+            normalizeWhitespace: true,
+          },
+        ],
+      }),
+  ].filter(Boolean),
+};
